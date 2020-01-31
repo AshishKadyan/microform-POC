ece7d473b2bca42f2cb4011da616d55a497073c6
Ashish <ashish.kadyan@comprotechnologies.com>
2020-01-31 11:42:45 +0530

    Changing URL

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ app.get('/checkout', function (req, res) {
 
                 var request = new cybersourceRestApi.GeneratePublicKeyRequest();
                 request.encryptionType = 'RsaOaep256';
-                request.targetOrigin = 'http://localhost:3000';
+                request.targetOrigin = 'https://flex-mocroform-poc.herokuapp.com/';
 
                 var options = {
                         'generatePublicKeyRequest': request
